12626f42d19d8682c350dcb21d79ea46e55fcf56
Sportsdawg1 <league@WorthyMini-6.attlocal.net>
2019-04-10 18:08:25 -0700

    some work :sunglasses:

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,23 +1,30 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String[] s=new String[5];
 		//2. print the third element in the array
-		
+		System.out.println(s[3]);
 		//3. set the third element to a different value
-		
+	s[3]="Hello";
 		//4. print the third element again
-		
+		System.out.println(s[3]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for (int i = 0; i < s.length; i++) {
+			System.out.println(s[i]);
+		}
 		
 		//6. make an array of 50 integers
-		
+		int[] i=new int[50];
 		//7. use a for loop to make every value of the integer array a random number
-		
+		for (int j = 0; j < i.length; j++) {
+			Random r=new Random();
+			i[j]=r.nextInt();
+		}
 		//8. without printing the entire array, print only the smallest number in the array
 		
 		//9 print the entire array to see if step 8 was correct
